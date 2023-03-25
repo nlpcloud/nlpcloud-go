87a98b038988bdd9428d98be5d7cc397abd05dce
Julien Salinas <all@juliensalinas.com>
2023-03-25 15:50:12 +0100

    Add is_instruct.

--- api.go
+++ api.go
@@ -225,6 +225,7 @@ type GenerationParams struct {
 	LengthPenalty      *float64  `json:"length_penalty,omitempty"`
 	BadWords           *[]string `json:"bad_words,omitempty"`
 	RemoveEndSequence  *bool     `json:"remove_end_sequence,omitempty"`
+	IsInstruct         *bool     `json:"is_instruct,omitempty"`
 }
 
 // Generation generates a block of text by contacting the API.
